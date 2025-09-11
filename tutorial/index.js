3e1a21f095aea223802e25a3d357e4b8aa1b6710
Sritam369 <chakrabarttysritam@gmail.com>
2025-09-11 22:23:46 +0530

    JavaScript

---
+++ tutorial/index.js
@@ -0,0 +1,45 @@
+/*let r=+prompt("enter radius"); // use +prompt to convert a string to number
+const pi=3.14;
+console.log("area of circle" + (pi*r*r));*/
+
+let n=25.4168;
+console.log(typeof n);
+console.log (n.toFixed(2));
+console.log(n.toPrecision(3));
+console.log(n.toString());
+console.log(n.toString(2));
+console.log(Math.sqrt(16));
+console.log(Math.cbrt(27));
+console.log(Math.ceil(Math.random()*6)); // to get random value from 1 to 6.
+console.log(Math.ceil(Math.random()*7)+6); // to get random value from 7 to 14.
+let name="sritam";
+console.log(`my name is name`);
+console.log(`my name is ${name}`);
+let str="JavaScript";
+console.log(str);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.charAt(2));
+console.log(str.indexOf('J'));
+console.log(str.lastIndexOf('a'));
+console.log(str.lastIndexOf('a'));
+console.log(str.slice(1,5));
+console.log(str.split('a'));
+console.log(str.split('a').length-1); // this gives how many a's are there in javascript
+console.log(str.split('a').length-1 + str.split('e').length-1+ str.split('i').length-1 +str.split('o').length-1+str.split('u').length-1);// to find how many vowels are there in javascript
+console.log(str.length- (str.split(' ').length-1+str.split('a').length-1 + str.split('e').length-1+ str.split('i').length-1 +str.split('o').length-1+str.split('u').length-1)) // to find no of consonants in javascript
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
