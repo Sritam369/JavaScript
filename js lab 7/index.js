c2c24db1c814bb944e7fb0f7a636f6e32b977bd0
Sritam369 <chakrabarttysritam@gmail.com>
2025-09-24 21:46:52 +0530

    sorting added

--- js lab 7/index.js
+++ js lab 7/index.js
@@ -1,4 +1,5 @@
 /*Write a 'JavaScript' program to store 10 elements in array and print them on a console take the inputs from the user.
+*/
 
 let size= +prompt("enter an size");
 let arr=[]
@@ -8,8 +9,9 @@ for(let i = 0;i<size;i++)
 }
 arr.forEach (function (e,i){
   console.log (e)
-})*/
+})
 /*Write a 'JavaScript' program to Separate Strings and Numbers and print array using the forEach method.
+*/
 
 // let size2= +prompt("enter size");
 let mixedArray=[12, "apple", 45, "banana", "cherry", 78, "orange"];
@@ -27,9 +29,9 @@ mixedArray.forEach(function (e,i){
    
 })
  console.log(number);
-    console.log(string);*/
+    console.log(string);
 
-/*Write a 'JavaScript' program to Filter Even and Odd Numbers from array in separate arrays.
+/*Write a 'JavaScript' program to Filter Even and Odd Numbers from array in separate arrays.*/
 
 let orgArray=[3,12,25,45,78];
 let even=[]; let odd=[];
@@ -42,12 +44,12 @@ let even=[]; let odd=[];
     }
  })
  console.log(even);
- console.log(odd);*/
+ console.log(odd);
 
 /*Write a 'JavaScript' program to print Sum of All array elements.
 
 your task is to take the numbers array and the elements from the user using the prompt() function.
-you have to find and print the sum of the all array elements.
+you have to find and print the sum of the all array elements.*/
 
  let numbers=[45,3,25,78,12];
 let sum=0;
@@ -55,3 +57,29 @@ numbers.forEach(function(e,i){
     sum=sum+e;
 })
 console.log(sum);
+
+/*Write a 'JavaScript' program to sort the numbers array using selection sort technique without using predefined function.*/
+
+let arr=[45,3,25,78,12];
+for(let i=0;i<arr.length-1;i++){
+    for(let j=0;j<arr.length-1-i;j++){
+        if(arr[j]>arr[j+1]){
+            let temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp
+        }
+    }
+}
+console.log(`Ascending Order: [${arr}]`);
+
+let arr2=[45,3,25,78,12];
+for(let i=0;i<arr2.length-1;i++){
+    for(let j=0;j<arr2.length-1-i;j++){
+        if(arr2[j]<arr2[j+1]){
+            let temp=arr2[j];
+            arr2[j]=arr2[j+1];
+            arr2[j+1]=temp
+        }
+    }
+}
+console.log(`Descending Order: [${arr2}]`);
