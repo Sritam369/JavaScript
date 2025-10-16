cce2677bf485ad8370770db7a61a0ef5dfbc51dc
Sritam369 <chakrabarttysritam@gmail.com>
2025-10-16 22:45:13 +0530

    project

---
+++ BOM tutorial/script.js
@@ -0,0 +1,8 @@
+function click(){
+    window.open(`https://www.google.com/?zx=1760460077978&no_sw_cr=1`);
+}
+window.navigator.mediaDevices.getUserMedia({
+    video:true,audio:true
+})
+document.getElementById('vd').srcObject=res;
+new MediaRecorder(res)
---
+++ lab project2/script.js
@@ -0,0 +1,38 @@
+let btn1=document.getElementById("btn1");
+let output=document.getElementById("output");
+function getDetails(){
+    fetch(`https://fakestoreapi.com/products`)
+  .then(response => response.json())
+  .then(data => displayData(data));
+}
+btn1.addEventListener('click',getDetails);
+function displayData(data){
+    output.textContent="";
+  let p1=document.createElement('p');
+  let pdata=data[0].title;
+  console.log(pdata)
+  p1.textContent=pdata;
+  output.append(p1);
+  
+}
+
+let p2=document.getElementById("p2");
+let product=document.getElementsByClassName("women"); // this is returning an array
+//console.log[...product] ---> this will give all the divs with class name women
+//console.log(product[0]) ---> this will give the first index value i.e the first div with class name women
+let product2=document.getElementsByClassName("product"); 
+let product3=document.getElementsByClassName("jewelery");  
+let right=document.getElementById("right");
+p2.addEventListener('click',function(e){
+  p2.style.backgroundColor="blue";
+  //product[0].style.display="none" --->we can also write like this
+  [...product].forEach((e,i)=>{ 
+     e.style.display="none";
+  })
+
+   [product2].forEach((e,i)=>{
+     e.style.display="none";  
+  })
+  
+
+})
